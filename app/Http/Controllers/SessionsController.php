7bdd0bf9c3f92cb5c56a069bb68703e7ab4d2f2e
remi <remi.nkjm@gmail.com>
2023-08-22 21:48:51 +0900

    セッションの作成

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -1,13 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class SessionsController extends Controller
 {
     public function create()
     {
-    return view('sessions.create');
+        return view('sessions.create');
+    }
+        
+    public function store(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->route('users.show', Auth::user());
+        } else {
+            return back()
+                ->withErrors(['message' => 'The email or password is incorrect, please try again'])
+                ->withInput();
+        }
     }
 }
